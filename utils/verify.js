dd617bb42fb7f91d78056e0ba59eb68e9a0d6215
Ge <57165451+Astr0-G@users.noreply.github.com>
2022-12-25 17:51:59 -0800

    Update verify.js

--- utils/verify.js
+++ utils/verify.js
@@ -1,6 +1,3 @@
-// we can't have these functions in our `helper-hardhat-config`
-// since these use the hardhat library
-// and it would be a circular dependency
 const { run } = require("hardhat")
 
 const verify = async (contractAddress, args) => {
